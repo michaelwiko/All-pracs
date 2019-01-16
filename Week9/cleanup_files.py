fd7eb1a12d43f1b4ba80241a3299158e30aef900
MicWik <michaelwidjaja.wiko@my.jcu.edu.au>
2019-01-16 12:58:29 +0800

    9 and 10

---
+++ Week9/cleanup_files.py
@@ -0,0 +1,34 @@
+import os
+
+
+def main():
+    """Cleanup inconsistent song lyrics file names."""
+
+    # Change to desired directory
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        print("Directory:", directory_name)
+        print("\tcontains subdirectories:", subdirectories)
+        print("\tand files:", filenames)
+        print("(Current working directory is: {})".format(os.getcwd()))
+
+        for filename in filenames:
+            new_name = get_fixed_filename(filename)
+            print("Renaming {} to {}".format(filename, new_name))
+
+            full_name = os.path.join(directory_name, filename)
+            new_name = os.path.join(directory_name, new_name)
+            os.rename(full_name, new_name)
+
+
+def get_fixed_filename(filename):
+    """Return a 'fixed' version of filename."""
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    return new_name
+
+def get_fixed_filename2(filename2):
+    new_name2 = filename2.replace(" ", "_").replace("tN", "t_N")
+    return new_name2
+
+
+main()
